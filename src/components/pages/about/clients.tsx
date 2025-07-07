a279b900050f1cf211e97cd09aef47ed2aa0a889
YutaKawamura <inheritans1026@gmail.com>
2025-07-07 15:28:50 +0900

    feat: 教授写真描画画面の作成

--- src/components/pages/about/clients.tsx
+++ src/components/pages/about/clients.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { PageTitle } from "@/components/ui/page-title";
 import styles from "./clients.module.css";
 import { useTranslations } from "next-intl";
 import { SectionHeader } from "@/components/ui/section-title";
+import Image from "next/image";
 
 export const TitleSection = () => {
   const t = useTranslations("about");
@@ -18,6 +21,29 @@ export const ProfessorSection = () => {
   return (
     <div className={styles.professorSection}>
       <SectionHeader label={t("title")} size="lg" />
+      <div className={styles.container}>
+        <div className={styles.professorImage}>
+          {/* 画像のサイズを調整 heightとwidthの比率を一定にする*/}
+          <Image
+            src="/about/professor.png"
+            alt="教授"
+            height={400}
+            width={400}
+            style={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
+        <div className={styles.containerRight}>
+          <div className={styles.professorName}>
+            <h3>{t("name")}</h3>
+          </div>
+          <div className={styles.professorDescription}>
+            <p>{t("mail")}</p>
+            <p>{t("description")}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
